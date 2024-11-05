fdbd6d4e226984b520b3940c0ec83d69c768dc27
wangjiahan <wangjiahan@wepie.com>
2024-11-05 14:32:50 +0800

    chore: log

--- src/log.rs
+++ src/log.rs
@@ -2,12 +2,8 @@ use tracing::{info, Level};
 use tracing_subscriber::fmt::writer::MakeWriterExt;
 
 pub fn init_logger() {
-    let info = tracing_appender::rolling::daily("logs", "info.log")
-        .with_max_level(Level::INFO)
-        .with_min_level(Level::INFO);
-    let error = tracing_appender::rolling::daily("logs", "error.log")
-        .with_max_level(Level::ERROR)
-        .with_min_level(Level::ERROR);
+    let info = tracing_appender::rolling::daily("logs", "info.log").with_max_level(Level::INFO);
+    let error = tracing_appender::rolling::daily("logs", "error.log").with_max_level(Level::ERROR);
     let stdout = std::io::stdout.with_max_level(Level::INFO);
 
     tracing_subscriber::fmt()
