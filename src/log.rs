b9606c349420cbe761f2f5ce8978b7ff862f2482
wangjiahan <wangjiahan@wepie.com>
2024-11-05 15:24:13 +0800

    feat: remove stdout in log

--- src/log.rs
+++ src/log.rs
@@ -4,10 +4,10 @@ use tracing_subscriber::fmt::writer::MakeWriterExt;
 pub fn init_logger() {
     let info = tracing_appender::rolling::daily("logs", "info.log").with_max_level(Level::INFO);
     let error = tracing_appender::rolling::daily("logs", "error.log").with_max_level(Level::ERROR);
-    let stdout = std::io::stdout.with_max_level(Level::INFO);
 
     tracing_subscriber::fmt()
-        .with_writer(stdout.and(info).and(error))
+        .with_writer(info)
+        .with_writer(error)
         .init();
 
     info!("hedon_bot started");
